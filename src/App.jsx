0bf98da28958483a0b22430f4ac3cea41f423f45
A1-mamun <1154almamun@gmail.com>
2024-03-16 10:06:01 +0600

    Recipe section header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
+import RecipeHeader from "./components/RecipeHeader/RecipeHeader";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <RecipeHeader></RecipeHeader>
     </>
   );
 }
---
+++ src/components/RecipeHeader/RecipeHeader.jsx
@@ -0,0 +1,14 @@
+const RecipeHeader = () => {
+  return (
+    <div className="container mx-auto font-lexend text-center my-20">
+      <h1 className="text-5xl font-semibold mb-5">Our Recipes</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus
+        vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum
+        mauris aenean neque.
+      </p>
+    </div>
+  );
+};
+
+export default RecipeHeader;
